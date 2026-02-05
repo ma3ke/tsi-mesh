2dd0d2334c0180c355e0fcd412619bee69306d7e
Marieke Westendorp <ma3ke.cyber@gmail.com>
2026-02-05 17:25:14 +0100

    Properly round values during writing

--- src/writer.rs
+++ src/writer.rs
@@ -1,6 +1,13 @@
 use crate::{Exclusion, Inclusion, Triangle, Tsi, Vertex};
 use std::io::{Result, Write};
 
+/// Round values to sufficient spatial precision.
+const fn round_to_precision(v: f32) -> f32 {
+    // A 1/1000th of a nanometer ought to be enough.
+    const PRECISION: f32 = 1e3;
+    (v * PRECISION).round() / PRECISION
+}
+
 pub trait WriteTsi {
     fn write(&self, writer: impl Write) -> Result<()>;
 }
@@ -10,15 +17,16 @@ impl WriteTsi for Tsi {
         // Front matter.
         {
             writeln!(writer, "version 1.1")?;
-            let [x, y, z] = self.dimensions;
-            writeln!(writer, "box {x:.3} {y:.3} {z:.3}",)?;
+            let [x, y, z] = self.dimensions.map(round_to_precision);
+            writeln!(writer, "box {x} {y} {z}",)?;
         }
 
         // Vertices.
         writeln!(writer, "vertex {}", self.vertices.len())?;
         for (i, vertex) in self.vertices.iter().enumerate() {
-            let Vertex { position: [x, y, z], domain } = vertex;
-            writeln!(writer, "{i} {x:.3} {y:.3} {z:.3} {domain}")?;
+            let Vertex { position, domain } = vertex;
+            let [x, y, z] = position.map(round_to_precision);
+            writeln!(writer, "{i} {x} {y} {z} {domain}")?;
         }
 
         // Triangles.
@@ -31,15 +39,17 @@ impl WriteTsi for Tsi {
         // Inclusions.
         writeln!(writer, "inclusion {}", self.inclusions.len())?;
         for (i, inclusion) in self.inclusions.iter().enumerate() {
-            let Inclusion { ty, vertex_index, vector: [vx, vy] } = inclusion;
+            let Inclusion { ty, vertex_index, vector } = inclusion;
+            let [vx, vy] = vector.map(round_to_precision);
             writeln!(writer, "{i} {ty} {vertex_index} {vx} {vy}",)?;
         }
 
         // Exclusions.
         writeln!(writer, "exclusion {}", self.exclusions.len())?;
-        for (i, exclusion) in self.exclusions.iter().enumerate() {
+        for (i, &exclusion) in self.exclusions.iter().enumerate() {
             let Exclusion { vertex_index, radius } = exclusion;
-            writeln!(writer, "{i} {vertex_index} {radius:.3}",)?;
+            let radius = round_to_precision(radius);
+            writeln!(writer, "{i} {vertex_index} {radius}",)?;
         }
 
         Ok(())
