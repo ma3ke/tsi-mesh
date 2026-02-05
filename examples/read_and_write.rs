985ab899c13395e430a3d41813876ec84a4c6241
Marieke Westendorp <ma3ke.cyber@gmail.com>
2026-02-05 19:51:59 +0100

    Improve the example

--- examples/read_and_write.rs
+++ examples/read_and_write.rs
@@ -3,14 +3,17 @@ use std::io::{BufReader, BufWriter};
 use tsi::{ReadTsi, Tsi, WriteTsi, reader::TsiError};
 
 fn main() -> Result<(), TsiError> {
-    let path = "membrane.tsi";
-    let file = std::fs::File::open(path)?;
+    let mut args = std::env::args().skip(1);
+    let path = args.next().unwrap_or("membrane.tsi".to_string());
+    let file = std::fs::File::open(&path)?;
     let reader = BufReader::new(file);
 
     // Read a tsi file.
+    let start = std::time::Instant::now();
     let mesh = Tsi::parse(reader)?;
 
-    println!("Successfully parsed {path:?}.");
+    let seconds = start.elapsed().as_secs_f64();
+    println!("Successfully parsed {path:?} in {seconds:.3} s.");
     println!("         box: {:?} nm", mesh.dimensions);
     println!("    vertices: {}", mesh.vertices.len());
     println!("   triangles: {}", mesh.triangles.len());
@@ -28,14 +31,21 @@ fn main() -> Result<(), TsiError> {
 
     // Write a tsi-formatted string.
     let mut buffer = Vec::new();
+    let start = std::time::Instant::now();
     mesh.write(&mut buffer)?;
     let s = String::from_utf8(buffer).expect("should be valid UTF-8");
-    println!("{s}");
+    let nlines = s.lines().count();
+    let seconds = start.elapsed().as_secs_f64();
+    println!("Successfully serialized {nlines} lines in {seconds:.3} s.");
 
     // Or to a file, directly.
-    let file = std::fs::File::create("output.tsi")?;
+    let path = &args.next().unwrap_or("output.tsi".to_string());
+    let file = std::fs::File::create(path)?;
     let mut writer = BufWriter::new(file);
+    let start = std::time::Instant::now();
     mesh.write(&mut writer)?;
+    let seconds = start.elapsed().as_secs_f64();
+    println!("Successfully wrote to {path:?} in {seconds:.3} s.");
 
     Ok(())
 }
